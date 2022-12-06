6b64253b7a6e9e1d3488506b23a51ae61faa297f
Tony Grosinger <tony@grosinger.net>
2022-12-06 08:40:02 -0800

    nfc: automatic formatting fixes

--- digitalocean-dynamic-ip.go
+++ digitalocean-dynamic-ip.go
@@ -94,7 +94,7 @@ type DOResponse struct {
 	} `json:"links"`
 }
 
-//GetConfig : get configuration file ~/.digitalocean-dynamic-ip.json
+// GetConfig : get configuration file ~/.digitalocean-dynamic-ip.json
 func GetConfig() ClientConfig {
 	cmdHelp := flag.Bool("h", false, "Show the help message")
 	cmdHelp2 := flag.Bool("help", false, "Show the help message")
@@ -145,7 +145,7 @@ func usage() {
 	))
 }
 
-//CheckLocalIPs : get current IP of server. checks both IPv4 and Ipv6 to support dual stack environments
+// CheckLocalIPs : get current IP of server. checks both IPv4 and Ipv6 to support dual stack environments
 func CheckLocalIPs() (ipv4, ipv6 net.IP) {
 	var ipv4String, ipv6String string
 	ipv4CheckURL := "https://api.ipify.org/?format=text"
@@ -201,7 +201,7 @@ func getURLBody(url string) (string, error) {
 	return string(body), nil
 }
 
-//GetDomainRecords : Get DNS records of current domain.
+// GetDomainRecords : Get DNS records of current domain.
 func GetDomainRecords(domain string) []DNSRecord {
 	ret := make([]DNSRecord, 0)
 	var page DOResponse
